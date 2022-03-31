ee697d414d111d68779a088a2d27e007363b0258
Nash4star <forrester.nash@gmail.com>
2022-03-31 16:11:46 -0400

    getting edit to work

--- src/components/pets/ShowPet.js
+++ src/components/pets/ShowPet.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react'
-import { getOnePet } from '../../api/pets'
+import { getOnePet, updatePet, removePet } from '../../api/pets'
 import { useParams } from 'react-router-dom'
-import { Spinner, Container, Card } from 'react-bootstrap'
+import { Spinner, Container, Card, Button } from 'react-bootstrap'
 import {showPetSuccess, showPetFailure} from '../shared/AutoDismissAlert/messages'
+import EditPetModal from './EditPetModal'
 
 const ShowPet = (props) => {
 
     const [pet, setPet] = useState(null)
+    const [modalOpen, setModalOpen] = useState(false)
+    const [updated, setUpdated] = useState(false)
     const {user, msgAlert} = props
     const { id } = useParams()
     console.log('id in showPet', id)
@@ -28,7 +31,7 @@ const ShowPet = (props) => {
                     variant: 'danger',
                 })
             })
-    }, [id])
+    }, [updated])
 
     if (!pet) {
         return (
@@ -41,20 +44,40 @@ const ShowPet = (props) => {
     }
 
     return (
-        <Container className="fluid">
-            <Card>
-                <Card.Header>{pet.fullTitle}</Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        <small>Age: {pet.age}</small><br/>
-                        <small>Type: {pet.type}</small><br/>
-                        <small>
-                            Adoptable: {pet.adoptable ? 'yes' : 'no'}
-                        </small>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </Container>
+        <>
+            <Container className="fluid">
+                <Card>
+                    <Card.Header>{pet.fullTitle}</Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            <small>Age: {pet.age}</small><br/>
+                            <small>Type: {pet.type}</small><br/>
+                            <small>
+                                Adoptable: {pet.adoptable ? 'yes' : 'no'}
+                            </small>
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button onClick={() => setModalOpen(true)} className="m-2" variant="warning">
+                            Edit Pet
+                        </Button>
+                        <Button className="m-2" variant="danger">
+                            Delete Pet
+                        </Button>
+
+                    </Card.Footer>
+                </Card>
+            </Container>
+            <EditPetModal 
+                pet={pet}
+                show={modalOpen}
+                user={user}
+                msgAlert={msgAlert}
+                triggerRefresh={() => setUpdated(prev => !prev)}
+                updatePet={updatePet}
+                handleClose={() => setModalOpen(false)}
+            />
+        </>
     )
 }
 
