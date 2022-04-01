e9d4b5c6ff6a3f32347ca0f7e7496e8cc3cd1faf
Nash4star <forrester.nash@gmail.com>
2022-04-01 09:32:56 -0400

    full CRUD for pets and toys

--- src/components/pets/ShowPet.js
+++ src/components/pets/ShowPet.js
@@ -1,17 +1,27 @@
 import React, {useState, useEffect} from 'react'
 import { getOnePet, updatePet, removePet } from '../../api/pets'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { Spinner, Container, Card, Button } from 'react-bootstrap'
 import {showPetSuccess, showPetFailure} from '../shared/AutoDismissAlert/messages'
 import EditPetModal from './EditPetModal'
+import ShowToy from '../toys/ShowToy'
+import GiveToyModal from '../toys/GiveToyModal'
+
+const cardContainerLayout = {
+    display: 'flex',
+    justifyContent: 'center',
+    flexFlow: 'row wrap'
+}
 
 const ShowPet = (props) => {
 
     const [pet, setPet] = useState(null)
     const [modalOpen, setModalOpen] = useState(false)
+    const [toyModalOpen, setToyModalOpen] = useState(false)
     const [updated, setUpdated] = useState(false)
     const {user, msgAlert} = props
     const { id } = useParams()
+    const navigate = useNavigate()
     console.log('id in showPet', id)
     // empty dependency array in useEffect to act like component did mount
     useEffect(() => {
@@ -32,6 +42,39 @@ const ShowPet = (props) => {
                 })
             })
     }, [updated])
+
+    const removeThePet = () => {
+        removePet(user, pet.id)
+            .then(() => {
+                msgAlert({
+                    heading: 'pet politely removed!',
+                    message: 'theyre gone',
+                    variant: 'success',
+                })
+            })
+            .then(() => {navigate(`/`)})
+            .catch(() => {
+                msgAlert({
+                    heading: 'something went wrong',
+                    message: 'that aint it',
+                    variant: 'danger',
+                })
+            })
+    }
+
+    let toyCards
+    if (pet) {
+        if (pet.toys.length > 0) {
+            toyCards = pet.toys.map(toy => (
+                // need to pass all props needed for updateToy func in edit modal
+                <ShowToy 
+                    key={toy._id} toy={toy} pet={pet} 
+                    user={user} msgAlert={msgAlert}
+                    triggerRefresh={() => setUpdated(prev => !prev)}
+                />
+            ))
+        }
+    }
 
     if (!pet) {
         return (
@@ -58,15 +101,21 @@ const ShowPet = (props) => {
                         </Card.Text>
                     </Card.Body>
                     <Card.Footer>
+                        <Button onClick={() => setToyModalOpen(true)} className="m-2" variant="info">
+                            Give Pet a Toy?
+                        </Button>
                         <Button onClick={() => setModalOpen(true)} className="m-2" variant="warning">
                             Edit Pet
                         </Button>
-                        <Button className="m-2" variant="danger">
+                        <Button onClick={() => removeThePet()}className="m-2" variant="danger">
                             Delete Pet
                         </Button>
 
                     </Card.Footer>
                 </Card>
+            </Container>
+            <Container style={cardContainerLayout}>
+                {toyCards}
             </Container>
             <EditPetModal 
                 pet={pet}
@@ -76,6 +125,14 @@ const ShowPet = (props) => {
                 triggerRefresh={() => setUpdated(prev => !prev)}
                 updatePet={updatePet}
                 handleClose={() => setModalOpen(false)}
+            />
+            <GiveToyModal
+                pet={pet}
+                show={toyModalOpen}
+                user={user}
+                msgAlert={msgAlert}
+                triggerRefresh={() => setUpdated(prev => !prev)}
+                handleClose={() => setToyModalOpen(false)}
             />
         </>
     )
